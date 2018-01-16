901b38c8fe0b0025e29368bf64c75129ae24bcd5
tonylixu@gmail.com <tonylixu@gmail.com>
2018-01-16 11:32:58 -0500

    Added register_node method blueprint

--- src/blockchain.py
+++ src/blockchain.py
@@ -2,6 +2,18 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
+
+    def register_nore(self, address):
+        """
+        Add a new node to the list of nodes
+
+        :param address: Address of node. Eg. 'http://192.168.0.1:5000'
+        """
+        # Prase the url and retrieve the network location part
+        # Add the network location part into nodes
+        pass
+
 
     def new_block(self):
         # Creates a new Block and adds it to the chain
