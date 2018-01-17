e9198ba19982ac654ea78edc93a5a86f0abbbdf8
tonylixu@gmail.com <tonylixu@gmail.com>
2018-01-16 21:57:14 -0500

    Updated the method explanation, added "how to implement" for each method

--- src/blockchain.py
+++ src/blockchain.py
@@ -6,20 +6,22 @@ class Blockchain(object):
 
     def register_nore(self, address):
         """
-        Add a new node to the list of nodes
+        Add a new node to the list of nodes.
+        How to implement:
+            - Prase the url address and retrieve the network location part
+            - Add the network location part into nodes
 
         :param address: Address of node. Eg. 'http://192.168.0.1:5000'
         :return None
         """
-        # Prase the url and retrieve the network location part
-        # Add the network location part into nodes
         pass
 
 
     def new_block(self, proof, previous_hash):
         """
-        Create a new Block in the Blockchain. A block contains the following
-        elements:
+        Create a new Block in the Blockchain. 
+        How to implement:
+            - A block contains the following elements:
         block= {
             'index': Integer, the length of block chain + 1, Eg. 10
             'timestamp': time in seconds since the epoch
@@ -36,8 +38,14 @@ class Blockchain(object):
     
     def new_transaction(self, sender, recipient, amount):
         """
-        Create a new transaction to go into the next mined Block. New
-        transaction will be appended to self.current_transactions
+        Create a new transaction to go into the next mined Block. 
+        How to implement:
+            - New transaction will be appended to self.current_transactions
+            - Sample transaction: {
+                'sender': sender,
+                'recipient': recipient,
+                'amount': amount,
+            }
 
         :param sender: Address of the Sender
         :param recipient: Address of the Recipient
@@ -52,11 +60,13 @@ class Blockchain(object):
     
     def valid_chain(self, chain):
         """
-        Determine if a given blockchain is valid. Traverse the whole chain, 
-        for each block, verify the 'previous_hash" is equal to the calculate 
-        hash of last_block. If they don't match, return False.
-        Then verify the proof of work is correct. Run valid_proof against 
-        current block and last block's proof
+        Determine if a given blockchain is valid. 
+        How to implement:
+            - Traverse the whole chain, for each block, 
+                verify the 'previous_hash" is equal to the calculate 
+                hash of last_block. If they don't match, return False.
+            - Then verify the proof of work is correct. Run valid_proof against 
+                current block and last block's proof
 
         :param chain: A blockchain
         :return: True if valid, False if not
@@ -65,15 +75,45 @@ class Blockchain(object):
 
     def resolve_conflict(self):
         """
+        This is the consensus algorithm, it resolves conflicts by
+        replacing the chain with the longest one in the network.
 
+        :return: True if our chain was replaced, False if not
         """
 
     @staticmethod
     def hash(block):
-        # Hashes a Block
+        """
+        Create a SHA-256 hash of block. 
+        How to implement:
+            - You can sort the block (which is a dictionary), 
+                then dump into a JSON format, run the hashlib.sha256()
+                function
+
+        :param block: Block
+        """
         pass
+
+    def proof_of_work(slef, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+            - Find a number p2 such that hash(p1p2) contains leading 4 zeroes
+            - p1 is the previous proof, p2 is the new proof
+            - You can call the valid_proof method
+        :param last_proof: previous proof
+        :return: proof
+        """
     
-    @property
-    def last_block(self):
-        # Returns the last Block in the chain
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof.
+        How to implement:
+            - Combine last_proof and proof into a string
+            - Calculate the SHA-256 hash of the string
+            - If string[:4] == '0000', return True, otherwise return False
+
+        :param last_proof: Previous Proof
+        :param proof: Current Proof
+        :return: True if correct, False if not.
+        """
         pass
