874370b8ee1c49208803329d849ceff2212a6d1e
jinglu-jlu <jlv@blockchain.lixu.ca>
2018-01-29 03:03:07 +0000

    Update comment

--- src/hash.py
+++ src/hash.py
@@ -4,10 +4,9 @@ import json
 def hash(block):
     """
     Creates SHA256 hash of a block
-    Params
-        block: dict
 
-    Return: string
+    param block: dict
+    return: string
     """
 
     # To have a consistent hash value, make sure that the dictionary is ordered    
