cb6973b41dccc53d4cf4d2639fbcb647587ce6f4
Tony Li Xu <tonylixu@gmail.com>
2018-04-19 21:47:33 -0400

    Add parser
    Added parser to main

--- src/blockchain.py
+++ src/blockchain.py
@@ -236,5 +236,12 @@ def full_chain():
     return jsonify(response), 200
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+    
     app.run(host="0.0.0.0", debug=True)
     # app.run(host="50.116.17.168", port=443, debug=True)
